f00dff554c2350cd789653311527b8818765b334
Charles <Charlesdevopz@gmail.com>
2024-05-07 08:18:33 +0100

    Add high and low to forecast

--- src/utils.js/des_forecast.js
+++ src/utils.js/des_forecast.js
@@ -13,7 +13,9 @@ const forecast = (latitude, longitude, callback) =>{
             callback('Unable to find location', undefined)
 
         } else {
-            callback(undefined, body.current.weather_descriptions[0] + ". it's currently " + body.current.temperature + ' degrees out. ' + 'there is a ' + body.current.feelslike + '% chance of rain. ' )
+            //console.log(body.current.weather_descriptions[0])
+            //callback(undefined, body.current.weather_descriptions[0] + ". it's currently " + body.current.temperature + ' degrees out. ' + 'there is a ' + body.current.feelslike + '% chance of rain. ' )
+            callback(undefined, body.current.weather_descriptions[0] + ". it's currently " + body.current.temperature + ' degrees out. ' + 'it feels like ' + body.current.feelslike + 'degrees out. The humidity is ' + body.current.humidity + '%.'  )
 
         }
 
